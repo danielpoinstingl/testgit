7c4079df6a8f39bf1970587c7ee82701e6701175
danielpoinstingl <daniel.poinstingl@gmx.at>
2024-04-22 09:40:48 +0200

    change branch daniel

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Newtext");
         System.out.println("change branch daniel");
+        System.out.println("change in brunch daniel");
     }
 }
